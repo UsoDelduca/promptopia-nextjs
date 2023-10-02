db1947a520288ec959db514b4f3dc4772d962c99
Rodolfo Delduca <usodelduca@gmail.com>
2023-10-03 11:59:27 +1300

    set up the actual form

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,5 +1,21 @@
-const Form = () => {
-  return <div>Form</div>
+import Link from 'next/link'
+
+const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
+  return (
+    <section className="w-full max-w-full flex-start flex-col">
+      <h1 className="head_text text-left">
+        <span className="blue_gradient">{type} Post</span>
+      </h1>
+      <p className="desc text-left max-w-md">
+        {type} and share amzing prompts with the world, and let your imagination
+        run wild with any Ai-powered platform
+      </p>
+      <form
+        onSubmit={handleSubmit}
+        className="mt-10 w-full max-w-2x1 flex flex-col gap-7 glassmorphism"
+      ></form>
+    </section>
+  )
 }
 
 export default Form
