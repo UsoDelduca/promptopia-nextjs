1e966462e4ddadbb8af82ea39f578df5e0344ec9
Rodolfo Delduca <usodelduca@gmail.com>
2023-10-03 12:28:39 +1300

    create input field and buttons

--- components/Form.jsx
+++ components/Form.jsx
@@ -26,6 +26,34 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
             className="form_textarea"
           ></textarea>
         </label>
+        <label>
+          <span className="font-satoshi font-semibold text-base text-gray-700">
+            Tag {`  `}
+            <span className="front-normal">
+              (#product, #webdevelopment, #idea)
+            </span>
+          </span>
+          <input
+            value={post.tag}
+            onChange={(e) => setPost({ ...post, tag: e.target.value })}
+            placeholder="#tag"
+            required
+            className="form_input"
+          ></input>
+        </label>
+        <div className="flex-end mx-3 mb-5 gap-4">
+          <Link href="/" className="text-gray-500 text-sm">
+            Cancel
+          </Link>
+          <button
+            type="submit"
+            disabled={submitting}
+            className="px-5 py-1.5 bg-primary-orange rounded-full text-white"
+          >
+            {' '}
+            {submitting ? `${type}...` : type}
+          </button>
+        </div>
       </form>
     </section>
   )
