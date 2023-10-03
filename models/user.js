eb89d73f417534febef48369cb5dd8ac801d3bf7
Rodolfo Delduca <usodelduca@gmail.com>
2023-10-03 15:43:26 +1300

    fix typos

--- models/user.js
+++ models/user.js
@@ -8,13 +8,13 @@ const UserSchema = new Schema({
   },
   username: {
     type: String,
-    required: [true, 'USername is required!'],
+    required: [true, 'Username is required!'],
     match: [
       /^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
       'Username invalid, it should contain 8-20 alphanumeric letters and be unique!',
     ],
   },
-  imsge: {
+  image: {
     type: String,
   },
 })
