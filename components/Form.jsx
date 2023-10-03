f510054b35352c48b275cbbbaf31bb8399da5f2d
Rodolfo Delduca <usodelduca@gmail.com>
2023-10-03 14:10:14 +1300

    fix typos

--- components/Form.jsx
+++ components/Form.jsx
@@ -7,8 +7,8 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
         <span className="blue_gradient">{type} Post</span>
       </h1>
       <p className="desc text-left max-w-md">
-        {type} and share amzing prompts with the world, and let your imagination
-        run wild with any Ai-powered platform
+        {type} and share amazing prompts with the world, and let your
+        imagination run wild with any Ai-powered platform
       </p>
       <form
         onSubmit={handleSubmit}
